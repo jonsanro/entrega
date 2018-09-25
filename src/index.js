a7b0e7700f1fb79ba664b3fbd6ae23cadb85357d
Jon San Roman <jonsanromansarasua@gmail.com>
2018-09-25 22:53:05 +0200

    create footer

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import './index.scss';
 import { makeHeader } from 'components/header/header-component';
 import { makeImage } from 'components/image/image-component';
 import { appendComponent } from 'utils/utils';
+import { makeFooter } from 'components/footer/footer-component';
 
 
 document.addEventListener('DOMContentLoaded', () => {
     const components = [
         makeHeader({ title: 'Keeping Playing' }),
-        makeImage({})
+        makeImage(),
+        makeFooter()
     ];
     appendComponent(document.body, components);
 });
