181fa3e9daf048f0b810ba6a1c7adc5f38d4d2f4
Jon San Roman <jonsanromansarasua@gmail.com>
2018-09-26 00:04:10 +0200

    create mock data

--- src/components/songs/songs-components.js
+++ src/components/songs/songs-components.js
@@ -1,16 +1,11 @@
 import { appendComponent } from 'utils/utils';
 import { makeSong } from 'components/song/song-components';
+import songsJSON from 'data/songs.json';
 
 export const makeSongs = () => {
     const songs = document.createElement('div');
     songs.classList.add('songs');
-    appendComponent(songs, [
-        makeSong({
-            title: 'Titulo random',
-            author: 'Autor random',
-            imageUrl: 'https://r.hswstatic.com/w_907/gif/podcasts/stuffyoushouldknow-podcasts-wp-content-uploads-sites-16-2014-03-disco600x350.jpg'
-        }),
-    ]);
+    appendComponent(songs, songsJSON.map(songData => makeSong(songData)));
     return songs;
 };
 
